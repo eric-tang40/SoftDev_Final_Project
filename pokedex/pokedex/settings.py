2b68e5ac441c081edda788963035257bb384f236
Eric T <erict@Erics-MacBook-Air-4.local>
2024-01-10 15:09:19 -0500

    adding more to boilerplate

--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "pokemon/static",  
+    BASE_DIR / "trainers/static",
+    BASE_DIR / "core/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
