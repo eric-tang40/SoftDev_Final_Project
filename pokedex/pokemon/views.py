cf49f6a6f5eb5dbe07b5d9d804f6d6fd2b899ebc
ty-c1016 <tylerchan586@gmail.com>
2024-01-12 14:27:37 -0500

    started views

--- pokedex/pokemon/views.py
+++ pokedex/pokemon/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Pokemon
+from django.views.generic import ListView
+
+class PokemonListView(ListView):
+    model = Pokemon
 
 def index(request):
     return render(request, 'pokemon/index.html')
