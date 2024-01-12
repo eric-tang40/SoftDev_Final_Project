20bfe54fbe7edb632f63774e80c30dfe849f766b
ty-c1016 <tylerchan586@gmail.com>
2024-01-12 14:20:10 -0500

    models for pokemon and trainers done

--- pokedex/pokemon/models.py
+++ pokedex/pokemon/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Type(models.Model):
-    type_name = models.CharField(primary_key = True)    
+    type_name = models.CharField(max_length = 50, primary_key = True)    
     #do you wanna do strengths, weaknesses, and immunities bc you might need to have multiple
 # Create your models here.
 
 class Pokemon(models.Model):
     dex_num = models.SmallIntegerField(primary_key = True)
-    name = models.ManyToManyField(Type)
-    type = models.CharField()
+    type = models.ManyToManyField(Type)
+    name = models.CharField(max_length = 50)
     
     hp = models.SmallIntegerField()
     attack = models.SmallIntegerField()
--- pokedex/trainers/models.py
+++ pokedex/trainers/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
 from pokemon.models import Pokemon
 
 class Trainer(models.Model):
-    name = models.CharField(primary_key = True)
-    preferred_type = models.CharField()
-    badge_name = models.CharField()
-    # team  = models.ForeignKey(Pokemon)
+    name = models.CharField(max_length = 50, primary_key = True)
+    preferred_type = models.CharField(max_length = 50)
+    badge_name = models.CharField(max_length = 100)
+    team  = models.JSONField()
     # we meed to wait for novillo documentation for this and types
 # Create your models here.
