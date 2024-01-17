c311161de82027ad5d50469ecda6c58cd2174a76
Eric T <erict@Erics-MacBook-Air-4.local>
2024-01-16 20:43:19 -0500

    adding template for secrets.json and .gitignore file

--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -11,9 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+import os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# JSON-based secrets module 
+with open(os.path.join(
+    BASE_DIR, 'basketball', 'secrets.json')) as f: 
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    '''Get the secret variable or return explicit exception.'''
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = 'Set the {0} environment variable'.format(setting) 
+    raise ImproperlyConfigured(error_msg)
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,11 +93,18 @@ WSGI_APPLICATION = 'pokedex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+with open('secrets.json') as secrets_file:
+    secrets = json.load(secrets_file)
+
+DATABASES = { 
+    "default": {
+    "ENGINE": "django.db.backends.postgresql", 
+    "NAME": get_secret('database_name'), 
+    "USER": get_secret('database_user'), 
+    "PASSWORD": get_secret('database_pwd'), 
+    "HOST": get_secret('database_host'), 
+    "PORT": get_secret('database_port'),
+    } 
 }
 
 
