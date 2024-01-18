e2e11c0cf6a4805bfd8b4ee87f6543b5d3960938
Eric T <erict@Erics-Air-4.lan>
2024-01-18 15:19:41 -0500

    populating trainers

--- pokedex/trainers/models.py
+++ pokedex/trainers/models.py
@@ -9,3 +9,51 @@ class Trainer(models.Model):
     # we meed to wait for novillo documentation for this and types
 # Create your models here.
 
+# Trainer.objects.create(
+#     name='Brock',
+#     preferred_type='Rock',
+#     badge_name='Boulder Badge',
+#     team=[]  
+# )
+
+# Trainer.objects.create(
+#     name='Misty',
+#     preferred_type='Water',
+#     badge_name='Cascade Badge',
+#     team=[]  
+# )
+
+# Trainer.objects.create(
+#     name='Lieutenant Surge',
+#     preferred_type='Electric',
+#     badge_name='Thunder Badge',
+#     team=[] 
+# )
+
+# Trainer.objects.create(
+#     name='Erika',
+#     preferred_type='Grass',
+#     badge_name='Rainbow Badge',
+#     team=[] 
+# )
+
+# Trainer.objects.create(
+#     name='Koga',
+#     preferred_type='Poison',
+#     badge_name='Soul Badge',
+#     team=[] 
+# )
+
+# Trainer.objects.create(
+#     name='Sabrina',
+#     preferred_type='Psychic',
+#     badge_name='Marsh Badge',
+#     team=[] 
+# )
+
+# Trainer.objects.create(
+#     name='Giovanni',
+#     preferred_type='Ground',
+#     badge_name='Earth Badge',
+#     team=[] 
+# )
